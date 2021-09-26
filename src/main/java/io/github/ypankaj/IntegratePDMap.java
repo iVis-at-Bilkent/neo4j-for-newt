b884d8e0818919a374edcf72d0c0a766a334598f
y-pankaj <pankaj.yadav.ece17@itbhu.ac.in>
2021-09-26 21:45:40 +0530

    Start main algo

--- src/main/java/io/github/ypankaj/IntegratePDMap.java
+++ src/main/java/io/github/ypankaj/IntegratePDMap.java
@@ -1,26 +1,36 @@
 package io.github.ypankaj;
 
-import java.util.ArrayList;
-
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Result;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class IntegratePDMap {
+
+	static Logger logger = LoggerFactory.getLogger(IntegratePDMap.class);
+
 	// This gives us a log instance that outputs messages to the
     // standard log, normally found under `data/log/console.log`
     @Context
     public Log log;
+    
+    @Context
+	public GraphDatabaseService db;
+    
+    @Context
+    public Transaction tx;
     
     public class Output {
 
@@ -38,86 +48,55 @@ public class IntegratePDMap {
      * @return  A RelationshipTypes instance with the relations (incoming and outgoing) for a given node.
      */
     @Procedure(value = "integratePDMap")
-    @Description("Merge PD maps on push to database command	")
-    public Stream<Output> pushToDatabase(@Name("sourceNode") Node node) {
-        /*
-         * Make graph from given data
-         * For each node get its parent node. 
-         * Match the node in the graph, check if the outgoing edges are same
-         * and adjacent nodes are same or not.
-         * If node not found then add node.
-         * 
-         * For Process nodes just check if the adjacent and edges are subset or not.
-         * 
-         * Finally check for edges. Get adjacent nodes and check if 
-         */
-    	
-    	Map<String, Object> propertyMap = node.getAllProperties();
-    	
-    	System.out.println("Inside function, ready to print properties");
-    	
-    	for(String key: propertyMap.keySet()) {
-    		System.out.print(key + " ");
-    		String value = propertyMap.get(key).toString();
-    		System.out.println(value);
-    	}
+    @Description("Merge PD maps on push to database command")
+    public Stream<Output> pushToDatabasePD(@Name("node") Node node) { 
+    	long id = Helper.getNodeId(node);
+    	String label = Helper.getNodeLabel(node);
+    	String entityName = Helper.getNodeEntityName(node);
+    	String[] UOI = Helper.getNodeUOI(node);
+        String[] stateVariables = Helper.getNodeStateVariables(node);
+        Boolean multimer = Helper.getNodeMultimer(node);
+        Boolean cloneMarker = Helper.getNodeCloneMarker(node);
+        String cloneLabel = Helper.getNodeCloneLabel(node);
+
+
+        // get parent node
+        Node parentNode = getParentNodeFromNodePD(node);
+
+        if(parentNode != null) {
+            logger.info("Node's parent label {}", Helper.getNodeLabel(parentNode));
+            logger.info("Node's parent entity name {}", Helper.getNodeEntityName(parentNode));
+        }
+        else {
+            logger.info("parentNode is null");
+        }
+        
+
+
     	
     	String returnStatement = "Operation pushToDatabase Done";
     	return Stream.of(new Output(returnStatement));
     }
 
-    /**
-     * Adds the distinct type of a relationship to the given List<String>
-     *
-     * @param list  the list to add the distinct relationship type to
-     * @param relationship  the relationship to get the name() from
-     */
-    private void AddDistinct(List<String> list, Relationship relationship){
-        AddDistinct(list, relationship.getType().name());
+    public Node getParentNodeFromNodePD(Node node) {
+        long id = Helper.getNodeId(node);
+        return getParentNodeFromNodeIdPD(id);
     }
 
-    /**
-     * Adds an item to a List only if the item is not already in the List
-     *
-     * @param list  the list to add the distinct item to
-     * @param item  the item to add to the list
-     */
-    private <T> void AddDistinct(List<T> list, T item){
-        if(!list.contains(item))
-            list.add(item);
-    }
+    public Node getParentNodeFromNodeIdPD(long id) {
+        String query = "MATCH (parentNode)<-[:belongs_to_compartment|belongs_to_submap|belongs_to_complex]-(m) WHERE id(m) = $id " +
+                        "RETURN parentNode";
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("id", id);
 
-    /**
-     * This is the output record for our search procedure. All procedures
-     * that return results return them as a Stream of Records, where the
-     * records are defined like this one - customized to fit what the procedure
-     * is returning.
-     * <p>
-     * These classes can only have public non-final fields, and the fields must
-     * be one of the following types:
-     *
-     * <ul>
-     *     <li>{@link String}</li>
-     *     <li>{@link Long} or {@code long}</li>
-     *     <li>{@link Double} or {@code double}</li>
-     *     <li>{@link Number}</li>
-     *     <li>{@link Boolean} or {@code boolean}</li>
-     *     <li>{@link Node}</li>
-     *     <li>{@link org.neo4j.graphdb.Relationship}</li>
-     *     <li>{@link org.neo4j.graphdb.Path}</li>
-     *     <li>{@link Map} with key {@link String} and value {@link Object}</li>
-     *     <li>{@link List} of elements of any valid field type, including {@link List}</li>
-     *     <li>{@link Object}, meaning any of the valid field types</li>
-     * </ul>
-     */
-    public static class RelationshipTypes {
-        // These records contain two lists of distinct relationship types going in and out of a Node.
-        public List<String> outgoing;
-        public List<String> incoming;
+        Result result = tx.execute(query, queryParams);
 
-        public RelationshipTypes(List<String> incoming, List<String> outgoing) {
-            this.outgoing = outgoing;
-            this.incoming = incoming;
+        Node parentNode = null;
+        if (result.hasNext()) {
+            Map<String, Object> row = result.next();
+            parentNode = (Node) row.get("parentNode");
         }
+
+        return parentNode;
     }
 }
--- src/test/java/io/github/ypankaj/IntegratePDMapTest.java
+++ src/test/java/io/github/ypankaj/IntegratePDMapTest.java
@@ -2,9 +2,6 @@ package io.github.ypankaj;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.AfterAll;
@@ -15,10 +12,17 @@ import org.junit.jupiter.api.TestInstance;
 import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Record;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.internal.InternalNode;
 import org.neo4j.harness.Neo4j;
 import org.neo4j.harness.Neo4jBuilders;
+import org.neo4j.procedure.Procedure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class IntegratePDMapTest {
@@ -26,19 +30,23 @@ public class IntegratePDMapTest {
     private static Driver driver;
     private Neo4j embeddedDatabaseServer;
 
+	static Logger logger = LoggerFactory.getLogger(IntegratePDMapTest.class);
+       
     @BeforeAll
     void initializeNeo4j() {
         this.embeddedDatabaseServer = Neo4jBuilders.newInProcessBuilder()
-                .withDisabledServer()
+                .withProcedure(Procedure.class)
                 .withProcedure(IntegratePDMap.class)
+                .withProcedure(apoc.create.Create.class)
+                .withDisabledServer()
                 .build();
 
-        this.driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
+        IntegratePDMapTest.driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
     }
 
     @AfterAll
     void closeDriver(){
-        this.driver.close();
+        IntegratePDMapTest.driver.close();
         this.embeddedDatabaseServer.close();
     }
 
@@ -50,61 +58,171 @@ public class IntegratePDMapTest {
     }
 
     
-    @Test
-    public void checkReturnStatement() {
-    	System.out.println("Inside");
-        final String nameOne = "INCOMING";
-        final String nameTwo= "OUTGOING";
+//     @Test
+//     public void checkReturnStatement() {
+//     	System.out.println("Inside");
+//         final String nameOne = "INCOMING";
+//         final String nameTwo= "OUTGOING";
 
-        // In a try-block, to make sure we close the session after the test
-        try(Session session = driver.session()) {
+//         // In a try-block, to make sure we close the session after the test
+//         try(Session session = driver.session()) {
         	
-        	Map<String,Object> n1 = new HashMap<>();
-        	n1.put( "name", "Andy" );
-        	n1.put( "position", "Developer" );
-        	n1.put( "awesome", true );
+//         	Map<String,Object> n1 = new HashMap<>();
+//         	n1.put( "name", "Andy" );
+//         	n1.put( "position", "Developer" );
+//         	n1.put( "awesome", true );
 
-        	Map<String,Object> n2 = new HashMap<>();
-        	n2.put( "name", "Michael" );
-        	n2.put( "position", "Developer" );
-        	n2.put( "children", 3 );
+//         	Map<String,Object> n2 = new HashMap<>();
+//         	n2.put( "name", "Michael" );
+//         	n2.put( "position", "Developer" );
+//         	n2.put( "children", 3 );
 
-        	Map<String,Object> params = new HashMap<>();
-        	List<Map<String,Object>> maps = new ArrayList<Map<String,Object>>();
-        	maps.add(n1);
-        	maps.add(n2);
+//         	Map<String,Object> params = new HashMap<>();
+//         	List<Map<String,Object>> maps = new ArrayList<Map<String,Object>>();
+//         	maps.add(n1);
+//         	maps.add(n2);
         	
-        	params.put( "props", maps );
+//         	params.put( "props", maps );
 
-        	String query =
-        	    "UNWIND $props AS properties" + "\n" +
-        	    "CREATE (n:Person)" + "\n" +
-        	    "SET n = properties" + "\n" +
-        	    "RETURN n";
+//         	String query =
+//         	    "UNWIND $props AS properties" + "\n" +
+//         	    "CREATE (n:Person)" + "\n" +
+//         	    "SET n = properties" + "\n" +
+//         	    "RETURN n";
 
-        	session.run( query, params );
+//         	session.run( query, params );
         	        	
             
-            //Execute our procedure against it.
-            Result result = session.run("MATCH (u:Person) CALL integratePDMap(u) YIELD out RETURN out");
+//             //Execute our procedure against it.
+//             Result result = session.run("MATCH (u:Person) CALL integratePDMap(u) YIELD out RETURN out");
 
-            while (result.hasNext()) {
-				Map<String, Object> row = result.next().asMap();
-				for(String key: row.keySet()) {
-					System.out.println(key);
-//					String key = name.toString();
-					String value = row.get(key).toString();
-					System.out.println(row.getClass().getName());
-					System.out.println(value);
-				}
+//             while (result.hasNext()) {
+// 				Map<String, Object> row = result.next().asMap();
+// 				for(String key: row.keySet()) {
+// 					System.out.println(key);
+// //					String key = name.toString();
+// 					String value = row.get(key).toString();
+// 					System.out.println(row.getClass().getName());
+// 					System.out.println(value);
+// 				}
 
-			}
+// 			}
             
+//             assertEquals(true, true);
+// //            assertEquals("Operation pushToDatabase Done", record.get("returnStatement"));
+//             //Get the incoming / outgoing relationships from the result
+// //            assertThat(record.get("incoming").asList(x -> x.asString())).containsOnly(expectedIncoming);
+// //            assertThat(record.get("outgoing").asList(x -> x.asString())).containsOnly(expectedOutgoing);
+//         }
+//     }
+
+    @Test
+    void testPushToDataBase() {
+        logger.info("Starting testPushToDatabase test");
+        try(Session session = driver.session()) {
+            Transaction tx = session.beginTransaction();
+            logger.info("Generating map");
+            generateMap("PD/NeuronalMuscleSignalling.json", "PD/PDQuery.json", tx);
+            // tx.commit();
+            logger.info("Map Generated");
+
+
+            String query = "MATCH (n) WHERE n.entityName = \"ChAT\" RETURN n";
+            Result result = tx.run(query);
+            Map<String, Object> record = result.next().asMap();
+
+            InternalNode queriedNode = (InternalNode) record.get("n");
+
+            logger.info("Node's entity name {}", queriedNode.asMap().get("entityName"));
+            
+            String queryIntegratePDMap = "MATCH (n) WHERE n.entityName = \"ChAT\" " +
+                                            "CALL integratePDMap(n) YIELD out RETURN out";
+            logger.info("Calling integratePDMap"); 
+            tx.run(queryIntegratePDMap);
+            logger.info("Call to integratePDMap over");
+                            
+            // function is still not complete so using this
             assertEquals(true, true);
-//            assertEquals("Operation pushToDatabase Done", record.get("returnStatement"));
-            //Get the incoming / outgoing relationships from the result
-//            assertThat(record.get("incoming").asList(x -> x.asString())).containsOnly(expectedIncoming);
-//            assertThat(record.get("outgoing").asList(x -> x.asString())).containsOnly(expectedOutgoing);
+        }
+    }
+
+    @Test
+    void checkIfNeuronalMuscleSignallingMapIsLoadedCorrectly() throws Exception {
+        String NeuronalMuscleSignallingFile = "src/test/resources/PD/NeuronalMuscleSignalling.json";
+        String NeuronalMuscleSignallingAsString = Helper.readFileAsString(NeuronalMuscleSignallingFile);
+
+        String queryLocation = "src/test/resources/PD/PDQuery.json";
+        String queryMapAsString = Helper.readFileAsString(queryLocation);
+        
+        Map<String, Object> NeuronalMuscleSignallingData = Helper.getMapFromJSONString(NeuronalMuscleSignallingAsString);
+        Map<String, Object> queryMap = Helper.getMapFromJSONString(queryMapAsString);
+
+        String query = (String) queryMap.get("query");
+
+        logger.info("Generating map");
+        System.out.println("Generating map");
+        System.out.println("this is here "+ logger.isInfoEnabled());
+		// generate TGF Beta Singalling Map
+        try(Session session = driver.session()) {
+        	session.run( query, NeuronalMuscleSignallingData );
+        }
+        logger.info("Map Generated");
+
+		// assert if node count is what is expected
+		try(Session session = driver.session()) {
+        	Result result = session.run("MATCH (n) RETURN COUNT(n) as NodeCount");
+			Record row = result.single();
+			int NodeCount = row.get("NodeCount").asInt();
+
+			assertEquals(48, NodeCount);
+        }
+
+		// assert if relationship count is what is expected
+		try(Session session = driver.session()) {
+        	Result result = session.run("MATCH ()-[r]->() RETURN COUNT(r) as RelCount");
+			Record row = result.single();
+			int RelCount = row.get("RelCount").asInt();
+
+			assertEquals(83, RelCount);
+        }
+    }
+
+    // @Test
+    // public void testGetParentNodeFromNodePD() {
+    //     try(Session session = driver.session()) {
+    //         Transaction tx = session.beginTransaction();
+    //         generateMap("PD/NeuronalMuscleSignalling.json", "PD/PDQuery.json", tx);
+    //         tx.commit();
+
+    //         String query = "MATCH (n) WHERE n.entityName = \"ChAT\" RETURN n";
+    //         Result result = tx.run(query);
+    //         Record record = result.next();
+    //         Node queriedNode = (Node) record.get("n");
+
+    //         Node parentNode = helper.getParentNodeFromNodePD(queriedNode, tx);
+    //         String entityName = helper.getNodeEntityName(parentNode);
+    //         System.out.println(entityName);
+    //         assertEquals("expected", entityName);
+    //     }
+        
+    // }
+
+    public void generateMap(String fileLocation, String queryLocation, Transaction tx) {
+        try {
+            String SBGNMapFile = "src/test/resources/" + fileLocation;
+            String SBGNMapAsString = Helper.readFileAsString(SBGNMapFile);
+
+            queryLocation = "src/test/resources/" + queryLocation;
+            String queryMapAsString = Helper.readFileAsString(queryLocation);
+            
+            Map<String, Object> SBGNMapData = Helper.getMapFromJSONString(SBGNMapAsString);
+            Map<String, Object> queryMap = Helper.getMapFromJSONString(queryMapAsString);
+
+            String query = (String) queryMap.get("query");
+
+            tx.run(query, SBGNMapData);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
